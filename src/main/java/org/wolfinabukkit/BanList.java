1c4b8e242fb63eecb145c870504187295d606683
Tschallacka <mdibbets@outlook.com>
2014-09-10 19:38:20 +0200

    updated banlist.java with fresh content

--- src/main/java/org/wolfinabukkit/BanList.java
+++ src/main/java/org/wolfinabukkit/BanList.java
@@ -1,1 +1,19 @@
+package org.wolfinabukkit
 
+import java.util.Date;
+import java.util.Set;
+// we need freshly drawn up comments here also
+public interface BanList
+    {
+    public enum Type
+        {
+        NAME,IP;
+        }
+    public Set<BanEntry> getBanEntries();
+    public BanEntry getBanEntry(String whichone);
+    public BanEntry addBan(String who,String why, String expirationdate, String sourceofban);
+    // could have called it unban... 
+    public void pardon(String who);
+    // check if a player is banned
+    public boolean isBanned(String who);
+    }
