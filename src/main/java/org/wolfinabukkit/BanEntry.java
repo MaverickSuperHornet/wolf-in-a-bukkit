9f15d23fb4857d6fd7c95e7347bbb41880eb31dd
Tschallacka <mdibbets@outlook.com>
2014-09-10 19:25:30 +0200

    updated BanEntry.java with content

--- src/main/java/org/wolfinabukkit/BanEntry.java
+++ src/main/java/org/wolfinabukkit/BanEntry.java
@@ -1,1 +1,18 @@
+package org.wolfinabukkit;
 
+import java.util.Date;
+// comments need still to be added. remember, the original comments
+// also fall under gpl, we need new comments.
+public interface BanEntry 
+    {
+    public String getTarget();
+    public Date getCreated();
+    public void setCreated(Date createdon);
+    public String getReason();
+    public void setReason(String whyididit);
+    public Date getExpiration();
+    public void setExpiration(Date badmilk);
+    public String getSource(); 
+    public void setSource(String thesource);
+    public void save();
+    }
