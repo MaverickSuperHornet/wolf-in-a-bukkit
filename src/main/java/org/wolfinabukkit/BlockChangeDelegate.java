c9c9784bf97b4d9bd2c8a0373a5dd1d1208b3e81
Tschallacka <mdibbets@outlook.com>
2014-09-10 19:57:51 +0200

    added content to blockchangedelegate

--- src/main/java/org/wolfinabukkit/BlockChangeDelegate.java
+++ src/main/java/org/wolfinabukkit/BlockChangeDelegate.java
@@ -1,1 +1,19 @@
+package org.wolfinabukkit
 
+// need new comments here too, who volunteers to write?
+public interface BlockChangeDelegate
+    {
+    public boolean isEmpty(int Xaxis, int Yaxis, int Zaxis);
+    public int getHeight();
+    // yea.... if we could stop using this that would be great
+    @Deprecated
+    public boolean setRawTypeId(int Xaxis, int Yaxis, int Zaxis, int newBlockID);
+    @Deprecated
+    public boolean setRawTypeIdAndData(int Xaxis, int Yaxis, int Zaxis, int newBlockID,int BlockData); 
+    @Deprecated
+    public boolean setTypeId(int Xaxis, int Yaxis, int Zaxis, int newBlockID);
+    @Deprecated
+    public boolean setTypeIdAndData(int Xaxis, int Yaxis, int Zaxis, int newBlockID, int BlockData);
+    @Deprecated
+    public int getTypeId(int Xaxis, int Yaxis, int Zaxis)
+    }
