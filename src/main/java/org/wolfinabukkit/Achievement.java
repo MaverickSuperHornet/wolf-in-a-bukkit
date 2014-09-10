46725595142c45c1e20b5a9f14bf72b9cd1f5533
Tschallacka <mdibbets@outlook.com>
2014-09-10 17:21:44 +0200

    Added art.java and modified Achievement a bit

--- src/main/java/org/wolfinabukkit/Achievement.java
+++ src/main/java/org/wolfinabukkit/Achievement.java
@@ -8,46 +8,45 @@ public enum Achievement
     {
     // Added idents to make it clearer what the line of inheritance is.
     OPEN_INVENTORY,
-        MINE_WOOD (OPEN_INVENTORY),
-            BUILD_WORKBENCH (MINE_WOOD),
-                BUILD_PICKAXE (BUILD_WORKBENCH),
-                    BUILD_BETTER_PICKAXE (BUILD_PICKAXE),
-                        BUILD_FURNACE (BUILD_PICKAXE),
-                            COOK_FISH (BUILD_FURNACE),
-                            ACQUIRE_IRON (BUILD_FURNACE),
-                                ON_A_RAIL (ACQUIRE_IRON),
-                                GET_DIAMONDS (ACQUIRE_IRON),
-                                    NETHER_PORTAL (GET_DIAMONDS),
-                                        GHAST_RETURN (NETHER_PORTAL),
-                                            GET_BLAZE_ROD (NETHER_PORTAL),
-                                                BREW_POTION (GET_BLAZE_ROD),
-                                                END_PORTAL (GET_BLAZE_ROD),
-                                                    THE_END (END_PORTAL),
-                                                        SPAWN_WITHER (THE_END),
-                                                            KILL_WITHER (SPAWN_WITHER),
-                                                                FULL_BEACON (KILL_WITHER),
-                                                    EXPLORE_ALL_BIOMES (END_PORTAL),
-                                    ENCHANTMENTS (GET_DIAMONDS),
-                                        OVERKILL (ENCHANTMENTS),
-                                        BOOKCASE (ENCHANTMENTS),
-                                    DIAMONDS_TO_YOU (GET_DIAMONDS),
-                BUILD_HOE (BUILD_WORKBENCH),
-                    MAKE_BREAD (BUILD_HOE),
-                    BAKE_CAKE (BUILD_HOE),
-                BUILD_SWORD (BUILD_WORKBENCH),
-                    KILL_ENEMY (BUILD_SWORD),
-                        SNIPE_SKELETON (KILL_ENEMY),
-                    KILL_COW (BUILD_SWORD),
-                        FLY_PIG (KILL_COW),
-                        BREED_COW (KILL_COW),
-    ;
+        MINE_WOOD(OPEN_INVENTORY),
+            BUILD_WORKBENCH(MINE_WOOD),
+                BUILD_PICKAXE(BUILD_WORKBENCH),
+                    BUILD_BETTER_PICKAXE(BUILD_PICKAXE),
+                        BUILD_FURNACE(BUILD_PICKAXE),
+                            COOK_FISH(BUILD_FURNACE),
+                            ACQUIRE_IRON(BUILD_FURNACE),
+                                ON_A_RAIL(ACQUIRE_IRON),
+                                GET_DIAMONDS(ACQUIRE_IRON),
+                                    NETHER_PORTAL(GET_DIAMONDS),
+                                        GHAST_RETURN(NETHER_PORTAL),
+                                            GET_BLAZE_ROD(NETHER_PORTAL),
+                                                BREW_POTION(GET_BLAZE_ROD),
+                                                END_PORTAL(GET_BLAZE_ROD),
+                                                    THE_END(END_PORTAL),
+                                                        SPAWN_WITHER(THE_END),
+                                                            KILL_WITHER(SPAWN_WITHER),
+                                                                FULL_BEACON(KILL_WITHER),
+                                                    EXPLORE_ALL_BIOMES(END_PORTAL),
+                                    ENCHANTMENTS(GET_DIAMONDS),
+                                        OVERKILL(ENCHANTMENTS),
+                                        BOOKCASE(ENCHANTMENTS),
+                                    DIAMONDS_TO_YOU(GET_DIAMONDS),
+                BUILD_HOE(BUILD_WORKBENCH),
+                    MAKE_BREAD(BUILD_HOE),
+                    BAKE_CAKE(BUILD_HOE),
+                BUILD_SWORD(BUILD_WORKBENCH),
+                    KILL_ENEMY(BUILD_SWORD),
+                        SNIPE_SKELETON(KILL_ENEMY),
+                    KILL_COW(BUILD_SWORD),
+                        FLY_PIG(KILL_COW),
+                        BREED_COW(KILL_COW);
     // private stuff here
     private final Achievement theparent;
     
     // set parent to null on initialisation
     private Achievement()
         {
-        theparent = null;
+        this.theparent = null;
         }
     // some guy is now your parent, deal with it bruce, his name is Alfred.
     private Achievement(Achievement someparent)
