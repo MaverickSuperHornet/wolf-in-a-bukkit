357ead9ae434b7e24ff4d707e1738faf46c0021c
AdamQpzm <a>
2014-09-11 11:12:41 +0100

    Updated BanList.java to conform to convention

--- src/main/java/org/wolfinabukkit/BanList.java
+++ src/main/java/org/wolfinabukkit/BanList.java
@@ -2,18 +2,22 @@ package org.wolfinabukkit;
 
 import java.util.Date;
 import java.util.Set;
+
 // we need freshly drawn up comments here also
-public interface BanList
-    {
-    public enum Type
-        {
-        NAME,IP;
-        }
+public interface BanList {
+    public enum Type {
+        NAME, IP;
+    }
+
     public Set<BanEntry> getBanEntries();
-    public BanEntry getBanEntry(String whichone);
-    public BanEntry addBan(String who,String why, String expirationdate, String sourceofban);
-    // could have called it unban... 
+
+    public BanEntry getBanEntry(String whichOne);
+
+    public BanEntry addBan(String who, String why, String expirationDate, String sourceOfBan);
+
+    // could have called it unban...
     public void pardon(String who);
+
     // check if a player is banned
     public boolean isBanned(String who);
-    }
+}
