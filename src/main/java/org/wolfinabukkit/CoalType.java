212bdfa1e4c87cc12a455e280c6db503eb1ccb8f
Tschallacka <mdibbets@outlook.com>
2014-09-11 08:41:18 +0200

    added coaltype although its an obsolete class... all accessor functions are deprecated

--- src/main/java/org/wolfinabukkit/CoalType.java
+++ src/main/java/org/wolfinabukkit/CoalType.java
@@ -1,1 +1,37 @@
+package org.wolfinabukkit;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+public enum CoalType 
+    {
+    COAL(0x0),
+    CHARCOAL(0x1);
+
+    private final byte coal;
+    private final static Map<Byte, CoalType> COALMAP_DATA = Maps.newHashMap();
+
+    private CoalType(final int coal) 
+        {
+        this.coal = (byte)coal;
+        }
+
+    static 
+        {
+        for(CoalType chunkof : values()) 
+            {
+            COALMAP_DATA.put(chunkof.coal, type);
+            }
+        }
+    @Deprecated
+    public byte getData() 
+        {
+        return coal;
+        }
+    @Deprecated
+    public static CoalType getByData(final byte data) 
+        {
+        return BY_DATA.get(data);
+        }
+    }
