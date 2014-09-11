fd5bb9c04953dfc224a3256343c60fa976f291ce
Tschallacka <mdibbets@outlook.com>
2014-09-11 08:35:40 +0200

    Added new files once again. interfaces are boring work... to bad they are needed to make it craftbukkit compatible heh

--- src/main/java/org/wolfinabukkit/Chunk.java
+++ src/main/java/org/wolfinabukkit/Chunk.java
@@ -1,1 +1,24 @@
+package org.wolfinabukkit;
 
+import org.wolfinabukkit.block.Block;
+import org.wolfinabukkit.block.BlockState;
+
+import org.wolfinabukkit.entity.Entity;
+
+public interface Chunk 
+    {
+    int getX();
+    int getZ();
+    World getWorld();
+    Block getBlock(int Xaxis,int Yaxis,int Zaxis);
+    ChunkSnapshot getChunkSnapshot();
+    ChunkSnapshot getChunkSnapshot(boolean includeBuildLimit, boolean biome, boolean rain);
+    Entity[] getEntities();
+    BlockState[] getTileEntities();
+    boolean isLoaded();
+    boolean load(boolean runGenerator);
+    boolean load();
+    boolean unload(boolean savetodisk, boolean safe);
+    boolean unload(boolean savetodisk);
+    boolean unload();
+    }
--- src/main/java/org/wolfinabukkit/ChunkSnapshot.java
+++ src/main/java/org/wolfinabukkit/ChunkSnapshot.java
@@ -1,1 +1,25 @@
+package org.wolfinabukkit;
 
+import org.wolfinabukkit.block.Biome;
+
+public interface ChunkSnapshot 
+    {
+    int getX();
+    int getZ();
+    String getWorldName();
+    int getBlockSkyLight(int Xaxis, int Yaxis, int Zaxis);
+    int getBlockEmittedLight(int Xaxis, int Yaxis, int Zaxis);
+    int getHighestBlockYAt(int Xaxis, int Zaxis);
+    Biome getBiome(int Xaxis, int Zaxis);
+    double getRawBiomeTemperature(int Xaxis, int Zaxis);
+    double getRawBiomeRainfall(int Xaxis, int Zaxis);
+    long getCaptureFullTime();
+    // see if a chunk of air is just empty
+    boolean isSectionEmpty(int Ychunkblock);
+
+    @Deprecated
+    int getBlockData(int Xaxis, int Yaxis, int Zaxis);
+    @Deprecated
+    int getBlockTypeId(int Xaxis, int Yaxis, int Zaxis);
+
+}
