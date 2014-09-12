d78ca27e72187a451dbc58fd9be86080cffd1490
Tschallacka <mdibbets@outlook.com>
2014-09-12 22:18:05 +0200

    Added Color.java Might be a bit buggy because of the inherintance

---
+++ src/main/java/org/bukkit/Color.java
@@ -0,0 +1,5 @@
+package org.bukkit;
+import org.gplbukkit.configuration.serialization.SerializableAs;
+@SerializableAs("Color")
+public final class Color extends org.gplbukkit.Color implements org.gplbukkit.configuration.serialization.ConfigurationSerializable {
+}
